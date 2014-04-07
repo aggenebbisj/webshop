797821cc76a90f76793cc426f76cd7a787710e42
Remko de Jong <aggenebbisj@gmail.com>
2014-04-07 23:49:17 +0200

    Added first unit test skeleton to trigger build

---
+++ src/test/java/nl/rdj/promoprofs/business/entity/ProductTest.java
@@ -0,0 +1,13 @@
+package nl.rdj.promoprofs.business.entity;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class ProductTest {
+    
+    @Test
+    public void should_test_something() {
+       fail(); 
+    }
+    
+}
